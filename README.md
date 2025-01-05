9bcbc2c34e755bc855cf154276392bfdea148c76
Jeremy Cohen <jcohen02@users.noreply.github.com>
2025-01-05 17:59:27 +0000

    porting configuration and url paths to Django 5.

--- README.md
+++ README.md
@@ -1,22 +1,33 @@
 # simple-django-example-app
-This is a simple example Django application designed to provide a template and an overview of the structure of a basic Django project with user authentication.
+This is a simple example Django application designed to provide a template and
+an overview of the structure of a basic Django project with user
+authentication.
 
-This has been developed as an introductory exemplar for a web application development module at Imperial College London. 
+This has been developed as an introductory exemplar for a web application
+development module at Imperial College London.
 
 #### Introductory Notes
 
-* This is not intended as an alternative to Django's excellent [documentation](https://docs.djangoproject.com/en/1.10/) which you should refer to for a much more detailed discussion of Django's features, usage and API.
+* This is not intended as an alternative to Django's excellent
+  [documentation](https://docs.djangoproject.com/en/5.1/) which you should
+  refer to for a much more detailed discussion of Django's features, usage and
+  API.
 
-* This example is based on Django version 1.11.
+* This example is based on Django version 5.1.4.
 
 ## Anatomy of a simple Django application
 
 WIP...
 
+# Changelog
+
+ - Jan 2025: Updated for Django 5.1.4 and Python 3.10.
+ - May 2017: Original example app implementation for Django 1.11
+
 ### Acknowldgements
 
 This example makes use of the following libraries/frameworks:
 
- * Django 1.11: [https://www.djangoproject.com](https://www.djangoproject.com)
+ * Django 5.1: [https://www.djangoproject.com](https://www.djangoproject.com)
  * Bootstrap: [http://getbootstrap.com](http://getbootstrap.com)
  * jQuery: [http://jquery.com](http://jquery.com)
--- simple_app/simple_app/urls.py
+++ simple_app/simple_app/urls.py
@@ -1,32 +1,42 @@
-"""simple_app URL Configuration
+"""
+URL configuration for simple_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import os
+# from django.contrib import admin
 from django.conf import settings
-from django.conf.urls import url
-from django.views.static import serve
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.urls import path
+# from django.views.static import serve
+
 from simple_app.views import IndexView, SignedInIndexView
 
 urlpatterns = [
-    #url(r'^admin/', include(admin.site.urls)),
-    url(r'^js/(?P<path>.*)$', serve, {'document_root': os.path.join(settings.STATIC_ROOT,'js')}),
-    url(r'^css/(?P<path>.*)$', serve, {'document_root': os.path.join(settings.STATIC_ROOT,'css')}),
-    url(r'^img/(?P<path>.*)$', serve, {'document_root': os.path.join(settings.STATIC_ROOT,'img')}),
-    url(r'^$', IndexView.as_view(), name="index"),
-    url(r'^userhome$', SignedInIndexView.as_view(), name="index_signedin"),
-    url(r'^accounts/login/$', auth_views.LoginView.as_view()),
-    url(r'^accounts/logout/$', auth_views.LogoutView.as_view()),
+    # path('admin/', admin.site.urls),
+    # re_path(r'^js/(?P<path>.*)$', serve,
+    #         {'document_root': settings.STATIC_ROOT / 'js'}),
+    # re_path(r'^css/(?P<path>.*)$', serve,
+    #         {'document_root': settings.STATIC_ROOT / 'css'}),
+    # re_path(r'^img/(?P<path>.*)$', serve,
+    #         {'document_root': settings.STATIC_ROOT / 'img'}),
+    path(r'', IndexView.as_view(), name="index"),
+    path(r'userhome', SignedInIndexView.as_view(), name="index_signedin"),
+    path(r'accounts/login/', auth_views.LoginView.as_view()),
+    path(r'accounts/logout/', auth_views.LogoutView.as_view()),
+] + [
+    static('js/', document_root=settings.STATIC_ROOT / 'js'),
+    static('css/', document_root=settings.STATIC_ROOT / 'css'),
+    static('img/', document_root=settings.STATIC_ROOT / 'img'),
 ]
